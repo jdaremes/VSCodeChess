de30be7035d7caaa3b3bde6ba4ea6c6a7deaf1aa
James Semerad <u0710024@umail.utah.edu>
2023-06-21 15:34:24 -0600

    Setting up Board class

--- Model/Board.cs
+++ Model/Board.cs
@@ -1,5 +1,29 @@
-﻿namespace BoardSetup;
-public class Class1
-{
+﻿using System.Reflection;
 
+namespace BoardSetup;
+public class Board
+{
+    private static String? FEN;
+    private readonly String defaultFEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+
+    public Board(bool isChess960) 
+    {
+        if (!isChess960) 
+        {
+            FEN = defaultFEN;
+        } else 
+        {
+            FEN = GenerateFEN();
+        }
+    }
+
+    public static String GenerateFEN() 
+    {
+        return "";
+    }
+
+    public static void Main() 
+    {
+
+    }
 }
